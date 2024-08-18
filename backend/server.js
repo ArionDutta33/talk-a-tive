335ce04d7082468152e45ab601cebcc5e2e301bf
ArionDutta33 <ariondutta333@outlook.com>
2024-08-18 20:50:45 +0530

    auth

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,16 @@ const express = require("express")
 const dotenv = require("dotenv")
 const { chats } = require("./data/data.js")
 const connectDB = require("./config/db.js")
-
+const userRoutes = require("./routes/user.routes.js")
 dotenv.config()
 connectDB()
 const app = express()
-
+app.use(express.json())
 app.get("/", (req, res) => {
     res.send("Api running")
 })
-app.get("/api/chat", (req, res) => {
-    res.send(chats)
-})
 
-app.get("/api/chat/:id", (req, res) => {
-    const singleChat = chats.find((c) => c._id === req.params.id)
-    res.send(singleChat)
-})
-
+app.use("/api/user", userRoutes)
 
 const PORT = process.env.PORT || 3000
 
